71cb04bb938bed38669d6f2c30d26e64f7ae269a
Eric Zhao <ericlzhao28@gmail.com>
2018-02-18 09:53:01 -0800

    Working marvel chat

--- marvelChat/utils.py
+++ marvelChat/utils.py
@@ -1,8 +1,10 @@
 import csv
 import random
 import speech_recognition as sr
+import pyttsx3;
 
 
+context = [None, None]
 super_heroes = []
 with open('marvel_chars.csv', 'r') as f:
   reader = csv.reader(f)
@@ -38,6 +40,21 @@ def match_superheros(text):
   return False, None
 
 
+def match_aff(text):
+  for match in ["yee", "yes", "go ahead", "ya"]:
+    if match in text:
+      return True, True
+  for match in ["no", "nah", "do not", "stop", "don't want"]:
+    if match in text:
+      return True, False
+  return False, False
+
+def match_pronoun(text):
+  for match in [" he ", " she ", " they ", "them", "him", "her"]:
+    if match in text + " ":
+      return True, None
+  return False, None
+
 def match_power(text):
   for match in ["powerful", "power", "strong"]:
     if match in text:
@@ -46,7 +63,7 @@ def match_power(text):
 
 
 def match_rank(text):
-  for match in ["is anyone", "anybody", "am i"]:
+  for match in ["is anyone", "anybody", "am i more", "am i less", "am i as"]:
     if match in text:
       return True, None
   return False, None
@@ -58,6 +75,12 @@ def match_who(text):
       return True, None
   return False, None
 
+
+def match_me(text):
+  for match in ["me", "i"]:
+    if match in text:
+      return True, None
+  return False, None
 
 def match_greeting(text):
   for greeting in ["hello", "hi", "how are you", "hey", "heyo"]:
@@ -74,9 +97,37 @@ def match_bye(text):
 
 
 def respond(text, my_power, my_name):
+  if (context[0] is not None) and match_aff(text)[0]:
+    _, detail = match_aff(text)
+    context[0] = None
+    if detail is True:
+      return "Great! I'll update the settings. Thanks for being a hero."
+    else:
+      return "That's sad to hear. Maybe next time!"
+
+  context[0] = None
+
+  if (context[1] is not None) and match_pronoun(text)[0]:
+    hero, power, story = context[1]
+    hero = hero[0].upper() + hero[1:]
+    match2, _ = match_who(text)
+    match3, _ = match_power(text)
+    if match2:
+      return story
+    if match3:
+      if my_power > power:
+        return "You're more powerful than " + hero + "!" \
+            " " + hero + " has nothing on your planet-saving powers."
+      elif my_power < power:
+        return hero + " is still more powerful than you." \
+            " " + hero + "'s " + str(power) + " power points tower over your " \
+            "mere " + str(my_power) + "."
+      return "Wow, you two are perfectly matched."
+
   match, details = match_superheros(text)
   if match:
     hero, power, story = details
+    context[1] = details
     hero = hero[0].upper() + hero[1:]
     match2, _ = match_who(text)
     if match2:
@@ -89,6 +140,9 @@ def respond(text, my_power, my_name):
           " " + hero + "'s " + str(power) + " power points tower over your " \
           "mere " + str(my_power) + "."
     return "Wow, you two are perfectly matched."
+
+  context[1] = None
+
   match, _ = match_power(text)
   if match:
     match2, _ = match_rank(text)
@@ -97,9 +151,11 @@ def respond(text, my_power, my_name):
           "than " + get_bad_heroes(my_power) + " and less powerful than" \
           " " + get_good_heroes(my_power) + "."
     else:
-      return "Your power score is " + str(my_power) + ". Gain more power by" \
-          " saving more power! Want to extend your comfortable temperature " \
+      context[0] = True
+      return "You are at " + str(my_power) + ". Gain more power by" \
+          " saving more power! Want to extend your temperature " \
           "range?"
+
   match, _ = match_greeting(text)
   if match:
     return "Hi " + my_name + "."
@@ -111,28 +167,35 @@ def respond(text, my_power, my_name):
 
 def handle(inp):
   inp = str(inp).strip().lower()
-  return respond(inp, 15, "Eric")
+  return respond(inp, 1000, "Eric")
 
+
+def voice():
+  engine = pyttsx3.init();
+  r = sr.Recognizer()
+  with sr.Microphone() as source:
+    audio = r.listen(source)
+    # recognize speech using Microsoft Bing Voice Recognition
+    BING_KEY = "42cfbf81a1894b8fab2c197e9b509bb4"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+    try:
+      text = r.recognize_bing(audio, key=BING_KEY)
+      print("You: ", text)
+      response = handle(text)
+      print(response)
+      engine.say(response)
+      engine.runAndWait()
+    except sr.UnknownValueError:
+      print("We can't hear you, can you speak louder?")
+    except sr.RequestError as e:
+      print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+
+def text():
+  text = input("You: ")
+  response = handle(text)
+  print(response)
 
 if __name__ == "__main__":
-  print("Hey")
+  print("Say something!")
   while True:
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-      print("Say something!")
-      audio = r.listen(source)
-			# recognize speech using Microsoft Bing Voice Recognition
-			BING_KEY = "42cfbf81a1894b8fab2c197e9b509bb4"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-			try:
-        text = r.recognize_bing(audio, key=BING_KEY)
-			except sr.UnknownValueError:
-				print("We can't hear you, can you speak louder?")
-				continue
-			except sr.RequestError as e:
-				print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-			  return
-			print("You: ", text)
-			response = handle(text)
-			print(response)
-			speech.say(response)
+    voice()
 
