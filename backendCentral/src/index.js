454c30fd0f1c0a900b985205ac30f2053f928264
suyashSax <suyash2099@gmail.com>
2018-02-18 01:26:07 -0800

    node stuff

--- backendCentral/src/index.js
+++ backendCentral/src/index.js
@@ -5,63 +5,78 @@ const getPixels = require("get-pixels")
 const schedule = require('node-schedule');
 
 var app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const ndarray = require("ndarray")
+const cors = require('cors');
 
 app.use(bodyParser.json())
 
-var userMode = null
+const PARTICLE = "https://api.particle.io/v1/devices/34005b000e51353532343635/"
+
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000'
+}));
+
+var userMode = "active"
 
 /******** THERMO SENSOR ********/
 
 // IR response
 
-app.post('/node/thermo', (req,res) => {
-    var img = req.body.img
-    var total = 0
-    var mean = 0
-    var length = img.length
-    var width = img[0].length
-    for (var i = 0; i < length; i++){
-        for (var j = 0; j < width; j++){
-            total += img[i][j]
-        }
-    }
+function sig(x, mean){
+    return 1.0/(1+Math.pow(2.718, (-1.0*x+mean)));
 
-    mean = total/(length * width)
+}
 
-    /*
-        1: No user in room, Empty
-        2: User in room, Active
-        3: User in room, Inactive
-    */
-
+app.get('http://gateway.marvel.com/', (req,res) => {
+    axios.get(())
 })
 
-
-/******** MOBILE APP ********/
-
-// GET: Client will make this call every 30 secs to get optimal temperature
-
-app.post('/node/temp', (req, res) => {
-    var setting = req.body.setting[userMode]
-
-    var request = {
-        alpha: 0.05,
-        beta: 0.3,
-        room_temp: setting.room_temp,
-        min_temp: setting.min_temp,
-        max_temp: setting.max_temp,
-    }
-
-    axios.post('/flask/temp', request)
-        .then((response) => {
-            var temps = response.temps
-            // var metadata = response.body.metadata
-            res.status(200).send({
-                temps,
-                metadata
+app.get('/thermo', (req,res) => {
+    axios({
+        url: (PARTICLE + 'top_infra'),
+        method: 'get',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'authorization': 'Bearer 2648e0d867f1f3ea045707dde1e98aede2d3d32e'
+        }
+    })
+    .then((res0) => {
+        axios({
+            url: (PARTICLE + 'middle_infra'),
+            method: 'get',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'authorization': 'Bearer 2648e0d867f1f3ea045707dde1e98aede2d3d32e'
+            }
+        })
+        .then((res1) => {
+            axios({
+                url: (PARTICLE + 'bottom_infra'),
+                method: 'get',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'authorization': 'Bearer 2648e0d867f1f3ea045707dde1e98aede2d3d32e'
+                }
             })
+            .then((res2) => {
+                var top = res0.data.result
+                var mid = res1.data.result
+                var bottom = res2.data.result
+                var fan = 0
+                if (res2.data.result > 1.1*res0.data.result){
+                    fan = 1
+                    console.log("FAN ON");
+                }
+
+                var temps = {top, mid, bottom}
+                var mean =  (res0.data.result + res1.data.result + res2.data.result)/3
+                console.log(temps, mean);
+                var sigmoid = {top: sig(temps.top, mean), mid: sig(temps.mid, mean), bottom: sig(temps.bottom, mean)}
+                res.status(200).send({fan, sigTop: sigmoid.top, sigMid: sigmoid.mid, sigBottom: sigmoid.bottom})
+            })
+        })
     })
 })
 
